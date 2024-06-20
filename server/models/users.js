9a969e204cc217bc07a79735e3ec7ea0d8f39d50
rachelkas <rachelikoskas@gmail.com>
2024-06-20 19:15:41 +0300

    add changes

--- server/models/users.js
+++ server/models/users.js
@@ -9,3 +9,4 @@ const User = mongoose.model('User', userSchema);
 
 export default User
 
+
--- server/validations/users.js
+++ server/validations/users.js
@@ -1,11 +1,25 @@
-import Joi from "joi";
+// import Joi from "joi";
 
-const validateUser = (user) => {
+// const validateUser = (user) => {
+//     const schema = Joi.object({
+//         userName: Joi.string().required(),
+//         email: Joi.string().email().required()
+//     });
+//     return schema.validate(user);
+// };
+
+// module.exports = { User, validateUser };
+
+
+
+import Joi from 'joi';
+export const validateCreateUser = (userInfo) => {
     const schema = Joi.object({
         userName: Joi.string().required(),
-        email: Joi.string().email().required()
+        email: Joi.string().email().required(),
     });
-    return schema.validate(user);
-};
+    return schema.validate(userInfo);
+}
 
-module.exports = { User, validateUser };
+
+module.exports = { User, validateCreateUser };
